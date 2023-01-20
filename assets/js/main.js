8783652258b6d3e75235aaf02c53967b8686e0fb
Muhammad Rizki Bahtiar <muhammadrizkibaht@gmail.com>
2023-01-20 20:16:39 +0700

    responsive menu

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// TOGGLE MENU
+
+const toggle = document.getElementById('nav-toggle'), navMenu = document.getElementById('nav-menu');
+
+toggle.addEventListener('click', function() {
+    navMenu.classList.toggle('top-[-1000%]')
+    navMenu.classList.toggle('top-10');
+})
